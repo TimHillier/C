07a9c44268ef2ff7f64d973fad3dbcb4fdfbbd1c
mildlywarm <Tim@Odin.local>
2016-04-11 09:49:10 -0700

    Updated the checks
    Surrounded the checks with while statements, to make them continuously
    check .

--- CC.c
+++ CC.c
@@ -15,45 +15,12 @@ int b = 0;
 //boolean to see if you took from right & left
 bool R = false; 
 bool L = false; 
-
-
-//file input? 
-//This isnt even relevant fuck this
-/* 
-char ch;
-char *p; 
-char file_name[25]="Right"; 
-
-
-FILE *fp; 
-
-fp= fopen(file_name,"r");
-
-if(fp== NULL){
-	perror("Error while opening the file.\n");
-	exit(EXIT_FAILURE); 	
-} 
-printf("The contents of %s file are:\n",file_name);
-
-while ( ( ch = fgetc(fp))!= EOF){
-	printf("%c",ch); 
-}
-fclose(fp); 
-
-
-
-//idk what im doing. 
-*/
-
-
-
-//lleft and rright can act as buffers maybe
-
-	
+bool flagr, flagl = true; 
+//lleft and rright can act as buffers maybe	
 // read from left and right, and only continue once it gets both values. 
 //cannot take 2 from left or right. has to be one from each side
 //set values to -1
-//
+
 
 
 
@@ -65,19 +32,30 @@ while(true){
 //maybe make these if statements do whiles? 
 //take input from right
 
+while(flagr){
+
+
 if(rright[0] < 0 && R == false){
 	//take input from right motor and set it into the right array
 	rright[0]=right[c];
+	flagr = false; 
 	R=true;
 }else{
+	flagr = true; 
 	//wait?
 }
+}
+
+while(flagl){}
 if(lleft[0]<0 && L == false){
 	//take input from the left motor and set it into the left array
 	lleft[0]=left[c];
+	flagl = false; 
 	L=true; 
 }else{
+	flagl = true; 
 	//wait?
+}
 }
 
 if(rright[0]>=0 && lleft[0]>=0 ){
@@ -102,7 +80,7 @@ if(rright[0]>=0 && lleft[0]>=0 ){
 //Like what happens if this happens? 
 //I dont think this will loop forever, once it gets past the if else x2 then it just stops if its
 //false. Need a way to check again. Maybe a do while? Should I make left and right seperate? 
-//Does it matter? yes it does, if it takes 2 from right, and assigns one of them left, then its no good. 
+//Does it matter? yes it does, if it takes 2r, and assigns one of them left, then its no good. 
 //Maybe put both if/else checks, into a while loop, and if its else, then set the thing to true, 
 //and if it is true, then set it to false
 //EXAMPLE
@@ -117,33 +95,25 @@ f(rright[0] < 0 && R == false){
 	//take input from right motor and set it into the right array
 	rright[0]=right[c];
 	R=true;
-	flag = false; 
+	flag = false; //Breaks out of the while loop for checking the side
 }else{
 	flag = true; 
 	//and it continues to loop, checking for right input. 
 	//but then what if it gets Left first? 
 	//need to figure that out
 	//but this should work. I like this actually
-	//Yes
-	
-
-
-
-	//wait?
+	//yes
+	}
 }
-
-
-
-
-}
-
-
-
-
-
 */
-
-
+//---------------------------------------
+//If i can make both left and right run at the same time that would be good. 
+//But again, what do i do if I get 2r? 
+//Do i need to count, if I need to count than thats different. 
+//that would change everything. 
+//Would it though? 
+//Just keep count. lol
+//shoudl the last if(61) havve a loop, or will it work not having a loop? 
 
 
 
